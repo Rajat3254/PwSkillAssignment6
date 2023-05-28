9e770cb37c1576fcb4568aaceaa67fe19236c560
Rajat Singh <singhr3234@gmail.com>
2023-05-28 18:37:49 +0530

    question 1 is added

---
+++ Question_1.java
@@ -0,0 +1,11 @@
+class Student{
+    String name;
+    int roll_no;
+}
+public class Question_1{
+    public static void main(String[] args){
+        Student one=new Student();
+        one.name="john";
+        one.roll_no=2;
+    }
+}
